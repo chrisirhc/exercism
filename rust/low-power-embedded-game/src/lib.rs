274ef62b4cef23e75a88ec095ef3fba14d581aef
Chris Chua <chris.sirhc@gmail.com>
2022-09-25 16:28:33 +0800

    simplified evens answer after reading the hint

--- rust/low-power-embedded-game/src/lib.rs
+++ rust/low-power-embedded-game/src/lib.rs
@@ -2,31 +2,13 @@ pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
     (dividend / divisor, dividend % divisor)
 }
 
-pub struct Evens<I> {
-    iter: I,
-}
-impl<I> Evens<I> {
-    pub fn new(iter: I) -> Evens<I> {
-        Evens { iter }
-    }
-}
-
-impl<I> Iterator for Evens<I>
-where
-    I: Iterator,
-{
-    type Item = <I as Iterator>::Item;
-
-    fn next(&mut self) -> Option<<I as Iterator>::Item> {
-        let result = self.iter.next();
-        // skip next item
-        self.iter.next();
-        result
-    }
-}
-
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    Evens::new(iter)
+    iter.enumerate().filter_map(|(i, val)| {
+        match i % 2 {
+            0 => Some(val),
+            _ => None
+        }
+    })
 }
 
 pub struct Position(pub i16, pub i16);
