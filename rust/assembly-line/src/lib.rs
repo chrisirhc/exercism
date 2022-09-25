2fca776764690518ed932ec2e8d3a243bf65a60a
Chris Chua <chris.sirhc@gmail.com>
2022-09-25 09:22:10 +0800

    add all initial exercises done yesterday

---
+++ rust/assembly-line/src/lib.rs
@@ -0,0 +1,14 @@
+const PRODUCED_PER_HOUR: f64 = 221.0;
+
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    let rate: f64 = match speed {
+        0..=4 => 1.0,
+        5..=8 => 0.9,
+        _ => 0.77,
+    };
+    PRODUCED_PER_HOUR * (speed as f64) * rate
+}
+
+pub fn working_items_per_minute(speed: u8) -> u32 {
+    (production_rate_per_hour(speed) / 60.0) as u32
+}
