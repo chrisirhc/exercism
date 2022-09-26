e0349d823d012ff535759aa1e4a85dfc35b5b6a0
Chris Chua <chris.sirhc@gmail.com>
2022-09-26 21:58:16 +0800

    updated solution with package in exercism

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,7 +1,11 @@
-use unicode_reverse::reverse_grapheme_clusters_in_place;
+use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let mut x = input.to_owned();
-    reverse_grapheme_clusters_in_place(&mut x);
-    x
+    let mut g = input.graphemes(true).collect::<Vec<&str>>();
+    g.reverse();
+    let mut s = String::new();
+    for i in g.iter() {
+        s = s + i;
+    }
+    s
 }
