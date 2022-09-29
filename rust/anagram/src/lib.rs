90b3b7ad49ad1bc0ce0087518f6d09755cd9f0b9
Chris Chua <chris.sirhc@gmail.com>
2022-09-29 21:10:35 +0800

    fix all the tests

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,26 +1,27 @@
 use std::collections::{HashMap, HashSet};
 
-fn count_letters(word: &str) -> HashMap<String, u8> {
+fn count_letters(word: &str) -> HashMap<char, u8> {
     let mut letter_counts = HashMap::new();
     for c in word.chars() {
-        let lowercase_c = c.to_lowercase().to_string();
-        let count = match letter_counts.get(&lowercase_c) {
+        let count = match letter_counts.get(&c) {
             Some(&count) => count,
             None => 0,
         };
-        letter_counts.insert(lowercase_c, count + 1);
+        letter_counts.insert(c, count + 1);
     }
     letter_counts
 }
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let count_for_word = count_letters(word);
+    let lowercase_word = word.to_lowercase();
+    let count_for_word = count_letters(&lowercase_word);
     let mut matched_words = HashSet::new();
     for &candidate in possible_anagrams.iter() {
-        if word.eq_ignore_ascii_case(candidate) {
+        let lowercase_candidate = candidate.to_lowercase();
+        if lowercase_word.eq(&lowercase_candidate) {
             continue;
         }
-        let count_for_candidates = count_letters(candidate);
+        let count_for_candidates = count_letters(&lowercase_candidate);
         if count_for_word.eq(&count_for_candidates) {
             matched_words.insert(candidate);
         }
