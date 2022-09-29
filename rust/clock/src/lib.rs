d59360bda0b12a772ebf2ef607bf934b0180691e
Chris Chua <chris.sirhc@gmail.com>
2022-09-29 18:45:44 +0800

    cleanup repeated logic

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -11,34 +11,36 @@ pub struct Clock {
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{:02}:{:02}",
-            self.minutes
-                .div_euclid(MINUTES_IN_AN_HOUR)
-                .rem_euclid(HOURS_IN_A_DAY),
-            self.minutes.rem_euclid(MINUTES_IN_AN_HOUR)
-        )
+        let (hours, minutes) = Clock::normalize(0, self.minutes);
+        write!(f, "{:02}:{:02}", hours, minutes)
     }
 }
 
 impl Clock {
-    fn normalize(hours: i32, minutes: i32) -> i32 {
+    fn normalize_to_minutes(hours: i32, minutes: i32) -> i32 {
+        Clock::to_minutes(Clock::normalize(hours, minutes))
+    }
+
+    fn normalize(hours: i32, minutes: i32) -> (i32, i32) {
         let new_minutes = minutes.rem_euclid(MINUTES_IN_AN_HOUR);
         let hours_carry_over = minutes.div_euclid(MINUTES_IN_AN_HOUR);
         let new_hours = (hours_carry_over + hours).rem_euclid(HOURS_IN_A_DAY);
-        new_hours * MINUTES_IN_AN_HOUR + new_minutes
+        (new_hours, new_minutes)
+    }
+
+    fn to_minutes((hours, minutes): (i32, i32)) -> i32 {
+        hours * MINUTES_IN_AN_HOUR + minutes
     }
 
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let new_minutes = Clock::normalize(hours, minutes);
+        let new_minutes = Clock::normalize_to_minutes(hours, minutes);
         Clock {
             minutes: new_minutes,
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let new_minutes = Clock::normalize(0, self.minutes + minutes);
+        let new_minutes = Clock::normalize_to_minutes(0, self.minutes + minutes);
         Clock {
             minutes: new_minutes,
         }
